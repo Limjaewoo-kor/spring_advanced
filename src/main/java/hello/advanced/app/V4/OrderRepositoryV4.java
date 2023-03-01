6ceddc725ec26d74ca02911c202e57c9dfdda897
Limjaewoo <ljw3944@naver.com>
2023-03-01 18:12:16 +0900

    Template Method Pattern

---
+++ src/main/java/hello/advanced/app/V4/OrderRepositoryV4.java
@@ -0,0 +1,42 @@
+package hello.advanced.app.V4;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+@Slf4j
+public class OrderRepositoryV4 {
+
+    private final LogTrace trace;
+
+
+    public void save(String itemId) {
+        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
+            @Override
+            protected Void call() {
+                //저장 로직
+                if( itemId.equals("ex")) {
+                    throw new IllegalArgumentException("예외발생!!");
+                }
+                sleep(1000);
+                return null;
+            }
+        };
+        template.execute("OrderRepository.save()");
+
+
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
